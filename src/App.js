826bd7e6589830480d2d7acf2d8f0cddb6df186f
Frederik Goris <fgoris@coursera.org>
2021-09-29 10:28:34 -0700

    install Emotion (and snapshot serializer support) and convert App to use Emotion (updating snapshot test)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
+/** @jsxImportSource @emotion/react */
+import { css, keyframes } from '@emotion/react';
 import logo from './logo.svg';
-import './App.css';
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const styles = {
+  app: css`
+    text-align: center;
+  `,
+  appHeader: css`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `,
+  appLogo: css`
+    height: 40vmin;
+    pointer-events: none;
+    @media (prefers-reduced-motion: no-preference) {
+      animation: ${appLogoSpin} infinite 20s linear;
+    }
+  `,
+  appLink: css`
+    color: #61dafb;
+  `,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div css={styles.app}>
+      <header css={styles.appHeader}>
+        <img src={logo} css={styles.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={styles.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- src/App.test.js
+++ src/App.test.js
@@ -11,15 +11,73 @@ test('renders learn react link', () => {
 test('renders', () => {
   const wrapper = shallow(<App />);
   expect(wrapper).toMatchInlineSnapshot(`
+    @keyframes animation-0 {
+      from {
+        -webkit-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -ms-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+
+      to {
+        -webkit-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -ms-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+
+    .emotion-0 {
+      text-align: center;
+    }
+
+    .emotion-1 {
+      background-color: #282c34;
+      min-height: 100vh;
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+      flex-direction: column;
+      -webkit-align-items: center;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      -webkit-justify-content: center;
+      justify-content: center;
+      font-size: calc(10px + 2vmin);
+      color: white;
+    }
+
+    .emotion-2 {
+      height: 40vmin;
+      pointer-events: none;
+    }
+
+    @media (prefers-reduced-motion: no-preference) {
+      .emotion-2 {
+        -webkit-animation: animation-0 infinite 20s linear;
+        animation: animation-0 infinite 20s linear;
+      }
+    }
+
+    .emotion-3 {
+      color: #61dafb;
+    }
+
     <div
-      className="App"
+      className="emotion-0"
     >
       <header
-        className="App-header"
+        className="emotion-1"
       >
         <img
           alt="logo"
-          className="App-logo"
+          className="emotion-2"
           src="logo.svg"
         />
         <p>
@@ -30,7 +88,7 @@ test('renders', () => {
            and save to reload.
         </p>
         <a
-          className="App-link"
+          className="emotion-3"
           href="https://reactjs.org"
           rel="noopener noreferrer"
           target="_blank"
