942cbc82d0b46a7931ad64a3b1a7a4fcaf92be9d
Frederik Goris <fgoris@coursera.org>
2021-09-29 09:41:19 -0700

    add a snapshot test and install and configure necessary packages to support snapshots (`enzyme-to-json` and `prettier`)

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,37 @@ test('renders learn react link', () => {
   expect(link).toHaveLength(1);
   expect(link.text()).toEqual(expect.stringMatching(/learn react/i));
 });
+
+test("renders", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchInlineSnapshot(`
+    <div
+      className="App"
+    >
+      <header
+        className="App-header"
+      >
+        <img
+          alt="logo"
+          className="App-logo"
+          src="logo.svg"
+        />
+        <p>
+          Edit 
+          <code>
+            src/App.js
+          </code>
+           and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  `);
+});
