b5be88e734f05038dad03aeaf55e394f582c578a
Frederik Goris <fgoris@coursera.org>
2021-09-29 09:29:01 -0700

    rewrite unit test in Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from "enzyme";
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  const link = wrapper.find('a');
+  expect(link).toHaveLength(1);
+  expect(link.text()).toEqual(expect.stringMatching(/learn react/i));
 });
